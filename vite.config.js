8bd96c94241137b6572e0f56dd9229c9198f4417
Promit Barua Antu <promitbarua796@gmail.com>
2026-09-12 02:08:45 +0600

    final(probably)

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,16 @@
 import { defineConfig } from "vite";
+import { resolve } from "path";
 
 export default defineConfig({
   base: "/geohazard/",
+
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        analysis: resolve(__dirname, "analysis.html"),
+        about: resolve(__dirname, "about.html"),
+      },
+    },
+  },
 });
